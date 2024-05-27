c5d1c1bd18bf3cd349e3b236893e696ce785d00a
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-05-27 20:57:07 -0300

    chore: host render config

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,16 @@
 import { knex as knexConfig, Knex } from 'knex'
 import { env } from './env'
 
+const connectionURL =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? {
+        filename: env.DATABASE_URL,
+      }
+    : env.DATABASE_URL
+
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection: connectionURL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
