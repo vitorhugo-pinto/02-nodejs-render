a8a5020f05846a7cdfb2abcb223a6139a165a1c1
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-05-27 21:09:43 -0300

    chore: host render config in app listen

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('it`s alive')
-})
+app
+  .listen({
+    port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+  })
+  .then(() => {
+    console.log('it`s alive')
+  })
